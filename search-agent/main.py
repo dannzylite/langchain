7c05a921698a0e629f4392f484d34fafe2fa79d4
Daniel <danielokorie3@gmail.com>
2026-07-13 15:50:26 +0100

    added base model

--- search-agent/main.py
+++ search-agent/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from dotenv import load_dotenv
 import os
 from langchain.agents import create_agent
@@ -6,8 +8,46 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from tavily import TavilyClient
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 load_dotenv()
+
+class Source(BaseModel):
+    """Schema for a source of information"""
+
+    url: str = Field(description="The source of the information")
+
+class AgentResponse(BaseModel):
+    """This is the schema for the agent response"""
+
+    answer: str = Field(description="The answer to the question")
+    sources: List[Source] = Field(default_factory=list, description="The sources of the information")
+
+
+
+
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+
+
+def main():
+    result = agent.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="search for 3 job posting for an ai engineeer using langchain in the bay area on linkedin and list their details"
+                )
+            ]
+        }
+    )
+    print(result['structured_response'].sources)
+
+
+if __name__ == "__main__":
+    main()
+
 
 # tavily = TavilyClient()
 
@@ -24,25 +64,3 @@ load_dotenv()
 
 #     print(f"Searching for: {query}")
 #     return tavily.search(query=query)
-
-
-tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-agent = create_agent(model=llm, tools=tools)
-
-
-def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                HumanMessage(
-                    content="search for 3 job posting for an ai engineeer using langchain in the bay area on linkedin and list their details"
-                )
-            ]
-        }
-    )
-    print(result)
-
-
-if __name__ == "__main__":
-    main()
